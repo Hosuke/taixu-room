454d1a398da0cb9b761e32bb500b1aee4c37a2f3
Huang Geyang <Sukebeta@outlook.com>
2026-08-13 13:09:26 +0800

    改用靜態 API 構建並復全站可讀

---
+++ assets/chunk-426QAEUC-4ZH3LIKG.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-ClppfGRW.js";import{n as t}from"./chunk-AGHRB4JF-BvmE3m-k.js";import{b as n}from"./chunk-ICPOFSXX-DKcUkC5X.js";var r=t(t=>{let{securityLevel:r}=n(),i=e(`body`);return r===`sandbox`&&(i=e((e(`#i${t}`).node()?.contentDocument??document).body)),i.select(`#${t}`)},`selectSvgElement`);export{r as t};
